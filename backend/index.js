7fd900ac5a6fb628a0ea3dc7af094c5e419d30f3
tushar patel <tushar2212099@akgec.ac.in>
2024-11-29 21:18:47 +0530

    back

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,40 @@
 import express from "express";
-import cookie from "cookie-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from './routes/user.routes.js'
-import companyRoute from './routes/company.route.js'
-import jobRoute from './routes/job.route.js'
-import applicationRoute from './routes/application.route.js'
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({})
+dotenv.config({});
 
-const app = express()
+const app = express();
 
-//middlewares
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookie());
-const corsOption = {
-  origin: ["http://localhost:5173","https://new-job-vista-tushar.vercel.app"],
-  credentials: true,
+app.use(cookieParser());
+const corsOptions = {
+    origin:['http://localhost:5173',"https://new-job-vista-tushar.vercel.app"],
+    credentials:true
 }
-app.use(cors(corsOption))
 
-const PORT =process.env.PORT || 1000;
+app.use(cors(corsOptions));
 
-//api's
-// http://localhost:8000/
+const PORT = process.env.PORT || 3000;
+
+
+// api's
 app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute); 
+app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT, ()=>{
-  connectDB();
-  console.log(`Server Started ${PORT}`);
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
 })
