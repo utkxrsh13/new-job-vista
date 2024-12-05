ac9b85c429a130a043282118b35aa9163757ada5
tushar patel <tushar2212099@akgec.ac.in>
2024-12-05 16:51:16 +0530

    origin cahnge

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://new-job-vista-tushar.vercel.app',
+    origin:'http://localhost:5173',
     credentials:true
 }
 
