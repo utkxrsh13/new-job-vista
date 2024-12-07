f5cf13fe2d3167649ac1af710d442c026a6af9f1
tushar patel <tushar2212099@akgec.ac.in>
2024-12-07 18:15:12 +0530

    file removed

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,11 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from "path";
 dotenv.config({});
 
 const app = express();
+// const dirname=path.resolve();
 
 // middleware
 app.use(express.json());
@@ -32,8 +33,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
+// app.use(express.static(path.join(dirname,"/frontend/dist")))
+// app.get('*',(_,res)=>{
+//     res.sendFile(path.resolve(dirname,"frontend","dist","index.html"))
+// })
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
