ea1e23a0f572b9edb5726fa19372f0c2b1a95120
utkxrsh13 <utkarsht0813@gmail.com>
2024-12-09 21:44:03 +0530

    user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,7 +3,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
-
 export const register = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
